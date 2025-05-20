64338af394cab6564a7acb6079ffe5cc11166571
jreitmei <johannes.reitmeier@emerson.com>
2025-05-20 17:04:37 +0200

    FFT implementation start

---
+++ gr-uhd/lib/rfnoc_fft_impl.cc
@@ -0,0 +1,292 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2022 Ettus Research, A National Instruments Brand.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "rfnoc_fft_impl.h"
+#include <gnuradio/io_signature.h>
+
+namespace gr {
+namespace uhd {
+
+namespace {
+    // Convert from GNU Radio to UHD
+    ::uhd::rfnoc::fft_direction fft_direction_gr2u(rfnoc_fft::fft_direction dir)
+    {
+        return static_cast<::uhd::rfnoc::fft_direction>(dir);
+    }
+
+    rfnoc_fft::fft_direction fft_direction_u2gr(::uhd::rfnoc::fft_direction dir)
+    {
+        return static_cast<rfnoc_fft::fft_direction>(dir);
+    }
+
+    ::uhd::rfnoc::fft_magnitude fft_magnitude_gr2u(rfnoc_fft::magnitude mag)
+    {
+        return static_cast<::uhd::rfnoc::fft_magnitude>(mag);
+    }
+    
+    rfnoc_fft::magnitude fft_magnitude_u2gr(::uhd::rfnoc::fft_magnitude mag)
+    {
+        return static_cast<rfnoc_fft::magnitude>(mag);
+    }
+
+    ::uhd::rfnoc::fft_shift fft_shift_gr2u(rfnoc_fft::fft_shift shift)
+    {
+        return static_cast<::uhd::rfnoc::fft_shift>(shift);
+    }
+
+    rfnoc_fft::fft_shift fft_shift_u2gr(::uhd::rfnoc::fft_shift shift)
+    {
+        return static_cast<rfnoc_fft::fft_shift>(shift);
+    }
+} // namespace
+
+rfnoc_fft::sptr rfnoc_fft::make(rfnoc_graph::sptr graph,
+                                      const ::uhd::device_addr_t& block_args,
+                                      const int device_select,
+                                      const int instance)
+{
+    return gnuradio::make_block_sptr<rfnoc_fft_impl>(rfnoc_block::make_block_ref(
+        graph, block_args, "FFT", device_select, instance));
+}
+
+
+rfnoc_fft_impl::rfnoc_fft_impl(::uhd::rfnoc::noc_block_base::sptr block_ref)
+    : rfnoc_block(block_ref),
+      d_fft_ref(get_block_ref<::uhd::rfnoc::fft_block_control>())
+{
+}
+
+rfnoc_fft_impl::~rfnoc_fft_impl() {}
+
+/******************************************************************************
+ * rfnoc_fft API
+ *****************************************************************************/
+
+
+// FFT Direction
+void rfnoc_fft_impl::set_direction(const rfnoc_fft::fft_direction direction)
+{
+    d_fft_ref->set_direction(fft_direction_gr2u(direction));
+}
+
+void rfnoc_fft_impl::set_direction(const std::string& direction)
+{
+    if (direction == "REVERSE") {
+        d_fft_ref->set_direction(::uhd::rfnoc::fft_direction::REVERSE);
+    } else if (direction == "FORWARD") {
+        d_fft_ref->set_direction(::uhd::rfnoc::fft_direction::FORWARD);
+    } else {
+        throw std::runtime_error(fmt::format(
+            "FFT direction ({:s}) is not [REVERSE, FORWARD]", direction));
+    }
+}
+
+rfnoc_fft::fft_direction rfnoc_fft_impl::get_direction() const
+{
+    return fft_direction_u2gr(d_fft_ref->get_direction());
+}
+
+std::string rfnoc_fft_impl::get_direction_string() const
+{
+    const auto direction = get_direction();
+    switch (direction) {
+    case rfnoc_fft::fft_direction::REVERSE:
+        return "REVERSE";
+    case rfnoc_fft::fft_direction::FORWARD:
+        return "FORWARD";
+    default:
+        throw std::runtime_error(fmt::format(
+            "FFT direction enumeration ({:d}) is not [REVERSE, FORWARD]", 
+            static_cast<uint32_t>(direction)));
+    }
+}
+
+// FFT Shift Configuration
+void rfnoc_fft_impl::set_shift_config(const rfnoc_fft::fft_shift shift)
+{
+    d_fft_ref->set_shift_config(fft_shift_gr2u(shift));
+}
+
+void rfnoc_fft_impl::set_shift_config(const std::string& shift)
+{
+    if (shift == "NORMAL") {
+        d_fft_ref->set_magnitude(::uhd::rfnoc::fft_magnitude::COMPLEX);
+    } else if (shift == "REVERSE") {
+        d_fft_ref->set_magnitude(::uhd::rfnoc::fft_magnitude::MAGNITUDE);
+    } else if (shift == "NATURAL") {
+        d_fft_ref->set_magnitude(::uhd::rfnoc::fft_magnitude::MAGNITUDE_SQUARED);
+
+    } else if (magnitude == "BIT_REVERSE") {
+        d_fft_ref->set_magnitude(::uhd::rfnoc::fft_magnitude::MAGNITUDE);
+    } else {
+        throw std::runtime_error(fmt::format(
+            "FFT shift configuration ({:s}) is not [NORMAL, REVERSE, NATURAL, BIT_REVERSE]", 
+            shift));
+    }
+}
+
+rfnoc_fft::fft_shift rfnoc_fft_impl::get_shift_config() const
+{
+    return fft_shift_u2gr(d_fft_ref->get_shift_config());
+}
+
+std::string rfnoc_fft_impl::get_shift_config_string() const
+{
+    const auto shift = get_shift_config();
+    switch (shift) {
+    case rfnoc_fft::fft_shift::NORMAL:
+        return "NORMAL";
+    case rfnoc_fft::fft_shift::REVERSE:
+        return "REVERSE";
+    case rfnoc_fft::fft_shift::NATURAL:
+        return "NATURAL";
+    case rfnoc_fft::fft_shift::BIT_REVERSE:
+        return "BIT_REVERSE";
+    default:
+        throw std::runtime_error(fmt::format(
+            "FFT shift configuration enumeration ({:d}) is not [NORMAL, REVERSE, NATURAL, BIT_REVERSE]", 
+            static_cast<uint32_t>(shift)));
+    }
+}
+
+
+// FFT Magnitude
+void rfnoc_fft_impl::set_magnitude(const rfnoc_fft::fft_magnitude magnitude)
+{
+    d_fft_ref->set_magnitude(fft_magnitude_gr2u(magnitude));
+}
+
+void rfnoc_fft_impl::set_magnitude(const std::string& magnitude)
+{
+    if (magnitude == "COMPLEX") {
+        d_fft_ref->set_magnitude(::uhd::rfnoc::fft_magnitude::COMPLEX);
+    } else if (magnitude == "MAGNITUDE") {
+        d_fft_ref->set_magnitude(::uhd::rfnoc::fft_magnitude::MAGNITUDE);
+    } else if (magnitude == "MAGNITUDE_SQUARED") {
+        d_fft_ref->set_magnitude(::uhd::rfnoc::fft_magnitude::MAGNITUDE_SQUARED);
+    } else {
+        throw std::runtime_error(fmt::format(
+            "FFT magnitude ({:s}) is not [COMPLEX, MAGNITUDE, MAGNITUDE_SQUARED]", 
+            magnitude));
+    }
+}
+rfnoc_fft::fft_magnitude rfnoc_fft_impl::get_magnitude() const
+{
+    return fft_magnitude_u2gr(d_fft_ref->get_magnitude());
+}
+
+std::string rfnoc_fft_impl::get_magnitude_string() const
+{
+    const auto magnitude = get_magnitude();
+    switch (magnitude) {
+    case rfnoc_fft::fft_magnitude::COMPLEX:
+        return "COMPLEX";
+    case rfnoc_fft::fft_magnitude::MAGNITUDE:
+        return "MAGNITUDE";
+    case rfnoc_fft::fft_magnitude::MAGNITUDE_SQUARED:
+        return "MAGNITUDE_SQUARED";
+    default:
+        throw std::runtime_error(fmt::format(
+            "FFT magnitude enumeration ({:d}) is not [COMPLEX, MAGNITUDE, MAGNITUDE_SQUARED]", 
+            static_cast<uint32_t>(magnitude)));
+    }
+}
+
+    // FFT Scaling Factor
+    void rfnoc_fft_impl::set_scaling_factor(const double factor)
+    {
+        d_fft_ref->set_scaling_factor(factor);
+    }
+
+    // FFT Scaling
+    void rfnoc_fft_impl::set_scaling(const uint32_t scaling)
+    {
+        d_fft_ref->set_scaling(scaling);
+    }
+
+    uint32_t rfnoc_siggen_impl::get_scaling()
+    {
+        return d_fft_ref->get_scaling();
+    }
+
+    //FFT Length
+    void rfnoc_fft_impl::set_length(const uint32_t length)
+    {
+        d_fft_ref->set_length(length);
+    }
+
+    uint32_t rfnoc_fft_impl::get_length() const
+    {
+        return d_fft_ref->get_length();
+    }
+
+    // FFT Bypass Mode
+    void rfnoc_fft_impl::set_bypass_mode(const bool bypass)
+    {
+        d_fft_ref->set_bypass_mode(bypass);
+    }
+
+    bool rfnoc_fft_impl::get_bypass_mode()
+    {
+        return d_fft_ref->get_bypass_mode();
+    }
+
+    // NIPC
+    uint32_t rfnoc_fft_impl::get_nipc() 
+    {
+        return d_fft_ref->get_nipc();
+    }
+
+    // Max Length
+    uint32_t rfnoc_fft_impl::get_max_length() 
+    {
+        return d_fft_ref->get_max_length();
+    }
+    // Max CP Length
+    uint32_t rfnoc_fft_impl::get_max_cp_length() 
+    {
+        return d_fft_ref->get_max_cp_length();
+    }
+    // Max CP Removal List Length
+    uint32_t get_max_cp_removal_list_length() 
+    {
+        return d_fft_ref->get_max_cp_removal_list_length();
+    }
+
+    // Max CP Insertion List Length
+    uint32_t get_max_cp_insertion_list_length() 
+    {
+        return d_fft_ref->get_max_cp_insertion_list_length();
+    }
+
+    // CP Insertion List
+    void rfnoc_fft_impl::set_cp_insertion_list(const std::vector<uint32_t>& cp_lengths)
+    {
+        d_fft_ref->set_cp_insertion_list(cp_lengths);
+    }
+
+    std::vector<uint32_t> rfnoc_fft_impl::get_cp_insertion_list() const
+    {
+        return d_fft_ref->get_cp_insertion_list();
+    }
+
+    // CP Removal List
+    void rfnoc_fft_impl::set_cp_removal_list(const std::vector<uint32_t>& cp_lengths)
+    {
+        d_fft_ref->set_cp_removal_list(cp_lengths);
+    }
+    std::vector<uint32_t> rfnoc_fft_impl::get_cp_removal_list() const
+    {
+        return d_fft_ref->get_cp_removal_list();
+    }
+
+    } // namespace uhd
+} // namespace gr
